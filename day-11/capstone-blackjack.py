3bb4504f92a6afa73e6983fd759d0fa5ce46b4ac
aintzevi <intz.katerina@gmail.com>
2022-06-15 20:07:12 +0200

    Add functions to draw card and calculate scores - handling of ace and formatting of score to be fixed

--- day-11/capstone-blackjack.py
+++ day-11/capstone-blackjack.py
@@ -1,7 +1,22 @@
 from art import logo
 import random
-
 import os
+
+
+def draw_card():
+    return random.choice(cards)
+
+
+def calculate_score(cards_list):
+    score = sum(cards_list)
+    if cards_list.__contains__(11) and score > 21:
+        cards_list[cards_list.index(11)] = 1
+        score -= 10
+    if score == 21:
+        score = 0
+
+    return score
+
 
 print(logo)
 
@@ -11,83 +26,60 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 continue_game = True
 
 while continue_game:
+    user_cards = []
+    computer_cards = []
     # Start user and computer hand with one card
-    user_cards = [random.choice(cards)]
-    computer_cards = [random.choice(cards)]
-    new_card_user = random.choice(cards)
-    new_card_computer = random.choice(cards)
+    for _ in range(2):
+        user_cards.append(draw_card())
+        computer_cards.append(draw_card())
 
-    user_sum = sum(user_cards)
-    computer_sum = sum(computer_cards)
+    draw_next_card = True
 
-    # TODO turn this check into helper function
-    # Change 11 to 1 if needed
-    if new_card_user == 11 and user_sum > 10:
-        new_card_user = 1
-    if new_card_computer == 11 and computer_sum > 10:
-        new_card_computer = 1
-
-    user_cards.append(new_card_user)
-    computer_cards.append(new_card_computer)
-
-    user_sum = sum(user_cards)
-    computer_sum = sum(computer_cards)
-
-    draw_card = True
-
-    while draw_card:
-        print(f"Your cards are {user_cards}, your current score {user_sum}")
+    while draw_next_card:
+        calculate_score(user_cards)
+        print(f"Your cards are {user_cards}, your current score {calculate_score(user_cards)}")
         print(f"Computer's first card: {computer_cards[0]}")
 
-        if user_sum == 21 and computer_sum == 21:
-            print(f"Computer's final hand is {computer_cards}, their final score {computer_sum}")
+        if calculate_score(user_cards) == 0 and calculate_score(computer_cards) == 0:
+            print(f"Computer's final hand is {computer_cards}, you both have a blackjack!")
             print("It it's a draw!")
             break
-        elif user_sum == 21:
-            print(f"Computer's final hand is {computer_cards}, their final score {computer_sum}")
-            print("You win!")
+        elif calculate_score(user_cards) == 0:
+            print(f"Computer's final hand is {computer_cards}, their final score {calculate_score(computer_cards)}.")
+            print("You win with a blackjack!")
             break
-        elif computer_sum == 21:
-            print(f"Computer's final hand is {computer_cards}, their final score {computer_sum}")
+        elif calculate_score(computer_cards) == 0:
+            print(f"Computer's final hand is {computer_cards}, they have a blackjack!")
             print("You lose!")
             break
-        elif user_sum > 21:
-            print(f"Computer's final hand is {computer_cards}, their final score {computer_sum}")
+        elif calculate_score(user_cards) > 21:
+            print(f"Computer's final hand is {computer_cards}, their final score {calculate_score(computer_cards)}")
             print("You drew over, you lose!")
             break
-        elif computer_sum > 21:
-            print(f"Computer's final hand is {computer_cards}, their final score {computer_sum}")
+        elif calculate_score(computer_cards) > 21:
+            print(f"Computer's final hand is {computer_cards}, their final score {calculate_score(computer_cards)}")
             print(f"Computer drew over, you win!")
             break
         else:
             while True:
                 hit = input("Type 'h' to hit or 's' to stand: ").lower()
                 if hit == 'h':
-                    new_card_user = random.choice(cards)
-                    if new_card_user == 11 and user_sum > 10:
-                        new_card_user = 1
-                    user_cards.append(new_card_user)
+                    user_cards.append(draw_card())
                     # The computer follows a policy of drawing when under 17
-                    if computer_sum < 17:
-                        new_card_computer = random.choice(cards)
-                        if new_card_computer == 11 and computer_sum > 10:
-                            new_card_computer = 1
-                        computer_cards.append(new_card_computer)
-                        computer_sum = sum(computer_cards)
-
-                    user_sum = sum(user_cards)
+                    if calculate_score(computer_cards) < 17:
+                        computer_cards.append(draw_card())
                     break
                 elif hit == 's':
-                    print(f"Your final hand is {user_cards}, your final score {user_sum}")
-                    print(f"Computer's final hand is {computer_cards}, their final score {computer_sum}")
+                    print(f"Your final hand is {user_cards}, your final score {calculate_score(user_cards)}")
+                    print(f"Computer's final hand is {computer_cards}, their final score {calculate_score(computer_cards)}")
 
-                    if user_sum < computer_sum:
+                    if calculate_score(user_cards) < calculate_score(computer_cards):
                         print("You lose!")
-                    elif user_sum > computer_sum:
+                    elif calculate_score(user_cards) > calculate_score(computer_cards):
                         print("You win!")
                     else:
                         print("It's a draw!")
-                    draw_card = False
+                    draw_next_card = False
                     break
                 else:
                     print("There is no option for this letter, try again!")
