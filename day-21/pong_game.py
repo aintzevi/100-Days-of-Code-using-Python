145feaaf83db2c8c6af085a602b224c0f5a26a38
aintzevi <intz.katerina@gmail.com>
2022-07-15 13:35:21 +0200

    Change window size, initial position of paddles, control for player 1 paddle

--- day-21/pong_game.py
+++ day-21/pong_game.py
@@ -1,21 +1,21 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 screen = Screen()
 screen.screensize(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen.bgcolor("black")
-screen.title("Pong game")
+screen.title("Pong")
 
 
 screen.tracer(0)
 player_1 = Paddle()
 player_2 = Paddle()
 
-player_1.goto(-SCREEN_WIDTH / 2 - 50, 0)
-player_2.goto(SCREEN_WIDTH / 2 + 50, 0)
+player_1.goto(-SCREEN_WIDTH / 2 + 20, 0)
+player_2.goto(SCREEN_WIDTH / 2 - 20, 0)
 
 screen.listen()
 
@@ -25,7 +25,7 @@ while game_is_on:
     screen.update()
 
     screen.onkey(player_1.up, "w")
-    screen.onkey(player_1.down, "a")
+    screen.onkey(player_1.down, "s")
 
     screen.onkey(player_2.up, "i")
     screen.onkey(player_2.down, "k")
