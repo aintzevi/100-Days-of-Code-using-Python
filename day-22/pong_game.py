01ce1e564f257ee80f36292040010eaad3eede07
aintzevi <intz.katerina@gmail.com>
2022-08-21 23:45:40 +0200

    Add scoreboard and score keeping when ball missed

--- day-22/pong_game.py
+++ day-22/pong_game.py
@@ -2,12 +2,13 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = Screen()
-screen.screensize(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.screensize(canvwidth=SCREEN_WIDTH, canvheight=SCREEN_HEIGHT)
 screen.bgcolor("black")
 screen.title("Pong")
 
@@ -17,6 +18,7 @@ l_player = Paddle((-SCREEN_WIDTH / 2 + 20, 0))
 r_player = Paddle((SCREEN_WIDTH / 2 - 20, 0))
 
 ball = Ball()
+scoreboard = Scoreboard(SCREEN_HEIGHT)
 
 screen.listen()
 # Paddle controls
@@ -29,12 +31,12 @@ game_is_on = True
 
 while game_is_on:
     # For the ball to go slower, use sleep. We could alternatively lower the amount by which the ball moves in each step
-    time.sleep(0.1)
+    # time.sleep(0.1)
     screen.update()
     ball.move()
 
     # Collision with up or down wall, meaning the ball should bounce
-    if ball.ycor() > 280 or ball.ycor() < -280:
+    if ball.ycor() > 290 or ball.ycor() < -290:
         ball.bounce_y()
 
     # Detect collision with the paddle (distance is not enough because of the paddle shape)
@@ -42,12 +44,15 @@ while game_is_on:
         ball.bounce_x()
 
     # Detect ball missing
-    if ball.xcor() > 380:
+    if ball.xcor() > 390:
         ball.reset_position()
         ball.bounce_x()
+        # Point goes to left player
+        scoreboard.score_point_left()
 
-    if ball.xcor() < -380:
+    if ball.xcor() < -390:
         ball.reset_position()
         ball.bounce_x()
+        scoreboard.score_point_right()
 
 screen.exitonclick()
--- day-22/scoreboard.py
+++ day-22/scoreboard.py
@@ -9,20 +9,28 @@ FONT_SIZE = 44
 class Scoreboard(Turtle):
     def __init__(self, screen_height):
         super().__init__()
-        self.score_1 = 0
-        self.score_2 = 0
+        self.score_left = 0
+        self.score_right = 0
 
         self.penup()
         self.hideturtle()
         self.goto(0, screen_height/2)
         self.color("white")
-        self.write(f"{self.score_1} \t {self.score_2}", align=ALIGNMENT, font=(FONT_NAME, FONT_SIZE, FONT_WEIGHT))
+        self.write(f"{self.score_left} \t {self.score_right}", align=ALIGNMENT, font=(FONT_NAME, FONT_SIZE, FONT_WEIGHT))
         time.sleep(0.5)
+
+    def score_point_left(self):
+        self.score_left += 1
+        self.refresh()
+
+    def score_point_right(self):
+        self.score_right += 1
+        self.refresh()
 
     def refresh(self):
         # TODO Update score values here
         self.clear()
-        self.write(f"{self.score_1} \t {self.score_2}", align=ALIGNMENT, font=(FONT_NAME, FONT_SIZE, FONT_WEIGHT))
+        self.write(f"{self.score_left} \t {self.score_right}", align=ALIGNMENT, font=(FONT_NAME, FONT_SIZE, FONT_WEIGHT))
 
     def game_over(self):
         self.goto(0, 0)
