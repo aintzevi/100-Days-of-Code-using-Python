54bc887582176b00db8db051eb77b850b7c0efce
aintzevi <intz.katerina@gmail.com>
2022-08-28 20:39:32 +0200

    Hide scoreboard turtle

--- day-23/scoreboard.py
+++ day-23/scoreboard.py
@@ -8,11 +8,11 @@ PLACEMENT = (-250, 250)
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.hideturtle()
         self.level = 1
 
         self.penup()
         self.setposition(PLACEMENT)
-        self.hideturtle()
         self.write(f"Level: {self.level}", True, "left", FONT)
         time.sleep(0.5)
 
