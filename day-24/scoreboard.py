5c895fcfc6fccef7d69aa2424f0ee96e2fd2d8f1
aintzevi <intz.katerina@gmail.com>
2022-08-29 20:29:15 +0200

    Add high score in the Scoreboard class and update for its value

--- day-24/scoreboard.py
+++ day-24/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,7 +16,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
@@ -23,5 +24,7 @@ class Scoreboard(Turtle):
 
     def increase_score(self):
         self.score += 1
+        if self.score > self.high_score:
+            self.high_score = self.score
         self.clear()
         self.update_scoreboard()
