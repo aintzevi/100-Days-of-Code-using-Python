2eb804e774c4dceb6e2588081bb468bc5df7b43e
aintzevi <intz.katerina@gmail.com>
2022-08-29 21:46:30 +0200

    Set constant for the letter placeholder

--- day-24/mail_merge/main.py
+++ day-24/mail_merge/main.py
@@ -1,3 +1,5 @@
+PLACEHOLDER = "[name]"
+
 # Keep the names in a list
 with open("Input/Names/invited_names.txt", mode="r") as letter:
     list_of_names = letter.readlines()
@@ -10,4 +12,4 @@ with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
         # Write/Create new file for each name
         with open(f"Output/ReadyToSend/letter_for_{name}", mode="w") as invitation:
             # Replace the placeholder with the name and remove leading/following white spaces from the name
-            invitation.write(letter_contents.replace("[name]", name.strip()))
+            invitation.write(letter_contents.replace(PLACEHOLDER, name.strip()))
