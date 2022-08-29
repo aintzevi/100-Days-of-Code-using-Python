005a27975cf8e8790cebf8ac2f58d3a4dd964448
aintzevi <intz.katerina@gmail.com>
2022-08-29 21:02:59 +0200

    Clear/Update scoreboard to display score/high score correctly

--- day-24/scoreboard.py
+++ day-24/scoreboard.py
@@ -16,12 +16,14 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
         self.score = 0
+        self.update_scoreboard()
 
     # def game_over(self):
     #     self.goto(0, 0)
