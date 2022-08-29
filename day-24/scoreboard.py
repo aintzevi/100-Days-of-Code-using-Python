d1e6c17d999ae608ceed12d80d771b57d0c1aa59
aintzevi <intz.katerina@gmail.com>
2022-08-29 20:31:42 +0200

    Reset score instead of gaming over when the snake dies

--- day-24/scoreboard.py
+++ day-24/scoreboard.py
@@ -18,13 +18,17 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        if self.score > self.high_score:
-            self.high_score = self.score
+
         self.clear()
         self.update_scoreboard()
