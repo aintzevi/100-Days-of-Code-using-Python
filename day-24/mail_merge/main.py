cec7c0de2044169b3e2d535463c691858f3c0535
aintzevi <intz.katerina@gmail.com>
2022-08-29 21:45:41 +0200

    Change code to get letter contents first, then iterate through the names

--- day-24/mail_merge/main.py
+++ day-24/mail_merge/main.py
@@ -2,11 +2,12 @@
 with open("Input/Names/invited_names.txt", mode="r") as letter:
     list_of_names = letter.readlines()
 
-# Iterate through the names list
-for name in list_of_names:
-    # Read the initial letter text
-    with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
+# Read the initial letter text
+with open("Input/Letters/starting_letter.txt", mode="r") as starting_letter:
+    letter_contents = starting_letter.read()
+    # Iterate through the names list
+    for name in list_of_names:
         # Write/Create new file for each name
         with open(f"Output/ReadyToSend/letter_for_{name}", mode="w") as invitation:
             # Replace the placeholder with the name and remove leading/following white spaces from the name
-            invitation.write(starting_letter.read().replace("[name]", name.strip()))
+            invitation.write(letter_contents.replace("[name]", name.strip()))
