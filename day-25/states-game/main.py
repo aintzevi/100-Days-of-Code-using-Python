1ca30f9115306434c625fc00219d3df8fccce1ef
aintzevi <intz.katerina@gmail.com>
2022-09-12 22:26:26 +0200

    Add basic map image, data retrieval from the csv and check for user correct answer to the game

--- day-25/states-game/main.py
+++ day-25/states-game/main.py
@@ -1,6 +1,35 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+image = "images/blank_states_img.gif"
+
+# Handle images in turtle
+turtle.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("data/50_states.csv")
+states = data.state.to_list()
+
+game_is_on = True
+
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the state: ", prompt="What's another state's name?").capitalize()
+    if answer_state in states:
+        # Specific row of answer state
+        state_data = data[data.state == answer_state]
+        city_name = turtle.Turtle()
+        city_name.penup()
+        city_name.hideturtle()
+        city_name.goto(int(state_data.x), int(state_data.y))
+        # We could also use state_data.state.item(), where item picks only the value of the data piece,
+        # without the data type information etc.
+        city_name.write(answer_state)
+
+    print(answer_state)
 
 screen.exitonclick()
+
+
+
